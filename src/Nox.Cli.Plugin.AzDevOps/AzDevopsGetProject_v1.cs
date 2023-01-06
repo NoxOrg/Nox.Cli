794bfb2431915962a8536d9a27c45a31b5a23d6b
jan <janschutte.lands@gmail.com>
2023-01-06 13:12:38 +0200

    Added AzDevopsGetProject_v1.cs

--- src/Nox.Cli.Plugin.AzDevOps/AzDevopsGetProject_v1.cs
+++ src/Nox.Cli.Plugin.AzDevOps/AzDevopsGetProject_v1.cs
@@ -1,18 +1,19 @@
 using Nox.Cli.Actions;
-using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.TeamFoundation.Core.WebApi;
+using Microsoft.VisualStudio.Services.Operations;
 using Microsoft.VisualStudio.Services.WebApi;
 
 namespace Nox.Cli.Plugins.AzDevops;
 
-public class AzDevopsGetRepo_v1 : INoxCliAddin
+public class AzDevopsGetProject_v1 : INoxCliAddin
 {
     public NoxActionMetaData Discover()
     {
         return new NoxActionMetaData
         {
-            Name = "azdevops/get-repo@v1",
+            Name = "azdevops/get-project@v1",
             Author = "Jan Schutte",
-            Description = "Get an Azure Devops repository",
+            Description = "Get an Azure Devops project",
 
             Inputs =
             {
@@ -22,40 +23,33 @@ public class AzDevopsGetRepo_v1 : INoxCliAddin
                     Default = new VssConnection(new Uri("https://localhost"), null),
                     IsRequired = true
                 },
+
                 ["project-name"] = new NoxActionInput { 
                     Id = "project-name", 
                     Description = "The DevOps project name",
                     Default = string.Empty,
                     IsRequired = true
                 },
-                ["repository-name"] = new NoxActionInput { 
-                    Id = "repository-name", 
-                    Description = "The DevOps repository name",
-                    Default = string.Empty,
-                    IsRequired = true
-                }
             },
 
             Outputs =
             {
-                ["repository"] = new NoxActionOutput {
-                    Id = "repository",
-                    Description = "The Azure devops repository",
+                ["project-id"] = new NoxActionOutput {
+                    Id = "project-id",
+                    Description = "The Id of the Azure devops project",
                 },
             }
         };
     }
 
-    private GitHttpClient? _repoClient;
-    private string? _repoName;
+    private ProjectHttpClient? _projectClient;
     private string? _projectName;
 
     public async Task BeginAsync(INoxWorkflowContext ctx, IDictionary<string,object> inputs)
     {
         var connection = (VssConnection)inputs["connection"];
-        _projectName = (string)inputs["project-name"]; 
-        _repoName = (string)inputs["repository-name"];
-        _repoClient = await connection.GetClientAsync<GitHttpClient>();
+        _projectName = (string)inputs["project-name"];
+        _projectClient = await connection.GetClientAsync<ProjectHttpClient>();
     }
 
     public async Task<IDictionary<string, object>> ProcessAsync(INoxWorkflowContext ctx)
@@ -64,30 +58,30 @@ public class AzDevopsGetRepo_v1 : INoxCliAddin
 
         ctx.SetState(ActionState.Error);
 
-        if (_repoClient == null || string.IsNullOrEmpty(_repoName) || string.IsNullOrEmpty(_projectName))
+        if (_projectClient == null || string.IsNullOrEmpty(_projectName))
         {
-            ctx.SetErrorMessage("The devops fetch-repo action was not initialized");
+            ctx.SetErrorMessage("The devops get-project action was not initialized");
         }
         else
         {
             try
             {
-                var repo = await _repoClient.GetRepositoryAsync(_projectName, _repoName);
-                outputs["repository"] = repo;
+                var project = await _projectClient.GetProject(_projectName);
+                outputs["project-id"] = project.Id;
             }
             catch
             {
-                outputs["repository"] = null!;
+                outputs["project-id"] = null!;
             }
-            ctx.SetState(ActionState.Success);
         }
-
+        
+        ctx.SetState(ActionState.Success);
         return outputs;
     }
 
     public Task EndAsync(INoxWorkflowContext ctx)
     {
-        _repoClient?.Dispose();
+        _projectClient?.Dispose();
         return Task.CompletedTask;
     }
 }
