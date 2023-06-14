9ed33dd3b667fc932f33f465742c6554a07c8c7e
Jan Schutte <JanSchutte.Lands@gmail.com>
2023-06-14 08:32:44 +0200

    moved workflos and templates into .nox

--- docs/scripts/template/88155c28-f750-4013-91d3-8347ddb3daa7/terraform/heimdall-ms-microservice.tf
+++
@@ -1,9 +0,0 @@
-module "heimdall_<app-snake-name>_workspaces" {
-  source = "./heimdall-ms-<app-dash-name>"
-
-  heimdall_workspace_preprod_we = module.Heimdall_Workspaces.workspaces["preprod_we"].name
-  heimdall_workspace_preprod_ne = module.Heimdall_Workspaces.workspaces["preprod_ne"].name
-  heimdall_workspace_prod_we    = module.Heimdall_Workspaces.workspaces["prod_we"].name
-  heimdall_workspace_prod_ne    = module.Heimdall_Workspaces.workspaces["prod_ne"].name
-
-}
--- docs/scripts/template/88155c28-f750-4013-91d3-8347ddb3daa7/terraform/outputs.tf
+++
@@ -1,6 +0,0 @@
-<test-we>
-<test-ne>
-<uat-we>
-<uat-ne>
-<prod-we>
-<prod-ne>
