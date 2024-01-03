792a961029f7deaa63d65e5786696411058ba542
Ricardo Rocha <rochar@gmail.com>
2024-01-03 10:49:37 +0000

    update ci to run and publish tests
    delete legacy tests
    add new Nox.Cli tests
    
    update sonar cloud steps

---
+++ tests/Nox.Cli.Tests/FixtureConfig/InlineAutoMoqDataAttribute.cs
@@ -0,0 +1,10 @@
+﻿using AutoFixture.Xunit2;
+
+namespace Nox.Cli.Tests.FixtureConfig;
+public class InlineAutoMoqDataAttribute : CompositeDataAttribute
+{
+    public InlineAutoMoqDataAttribute(params object[] values)
+        : base(new InlineDataAttribute(values), new AutoMoqDataAttribute())
+    {
+    }
+}
