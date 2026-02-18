8853742e1b2cae860273ce44ad28c5615fcf60f8
Carlos Pedreira <carlospedreira@outlook.com>
2026-02-18 16:50:54 +0000

    updated dotnet version to 10.0

--- src/Nox.Cli.Abstractions/Secrets/IPersistedSecretStoreEx.cs
+++ src/Nox.Cli.Abstractions/Secrets/IPersistedSecretStoreEx.cs
@@ -2,7 +2,7 @@ using Nox.Secrets.Abstractions;
 namespace Nox.Cli.Abstractions.Secrets;
 public interface IPersistedSecretStoreEx: IPersistedSecretStore
 {
-#if NET8_0    
+#if NET8_0 || NET10_0
     Task SaveAsync(string key, string secret);
     Task<string?> LoadAsync(string key, TimeSpan? validFor = null);
 #endif
